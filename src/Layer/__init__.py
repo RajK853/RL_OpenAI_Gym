9612112117288ba063dc29d44482241b04f93db0
Raj K <rgk.rana@gmail.com>
2020-08-02 02:17:02 +0200

    Minor changes

--- src/Layer/__init__.py
+++ src/Layer/__init__.py
@@ -5,4 +5,4 @@ ROOT_DIR = os.path.dirname(os.path.abspath(os.path.join(os.path.abspath(""), "..
 sys.path.insert(0, ROOT_DIR)
 
 from .neural_network import NeuralNetwork
-from .q_network import QNetwork
+from .qnetwork import QNetwork
--- src/Policy/__init__.py
+++ src/Policy/__init__.py
@@ -1,3 +1,13 @@
-from .base_policy import BasePolicy, UniformPolicy
+from .base_policy import BasePolicy
+from .discrete import DiscretePolicy
 from .gaussian import GaussianPolicy
 from .greedy_epsilon import GreedyEpsilonPolicy
+
+POLICIES = {"greedy_epsilon": GreedyEpsilonPolicy,
+            "gaussian": GaussianPolicy,
+            "discrete": DiscretePolicy,}
+
+
+def get_policy(name, **kwargs):
+    policy_func = POLICIES[name]
+    return policy_func(**kwargs)
