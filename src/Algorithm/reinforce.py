437b60fe65312c0b964749ff197441833b4aa693
Raj K <rgk.rana@gmail.com>
2020-08-03 13:08:41 +0200

    - Epoch trajectory stored in a dictionary with different list for each component - Minor refactoring

--- src/Algorithm/reinforce.py
+++ src/Algorithm/reinforce.py
@@ -8,18 +8,19 @@ class Reinforce(RLAlgorithm):
     def __init__(self, *, gamma=0.99, num_train=1, **kwargs):
         super(Reinforce, self).__init__(**kwargs)
         self.gamma = gamma
-        self.trajectory = []
+        self.trajectory = {}
         self.num_train = num_train
         self.mean_policy_loss = 0.0
         self.scalar_summaries += ("mean_policy_loss", )
 
+    def add_transition(self, **kwargs):
+        for key, value in kwargs.items():
+            if key not in self.trajectory.keys():
+                self.trajectory[key] = []
+            self.trajectory[key].append(value)
+
     def sample_trajectory(self):
-        states, actions, rewards = [], [], []
-        for (state, action, reward) in self.trajectory:
-            states.append(np.array(state, copy=False))
-            actions.append(np.array(action, copy=False))
-            rewards.append(np.array(reward, copy=False))
-        return np.array(states), np.array(actions), np.array(rewards)
+        return {key: np.array(value) for key, value in self.trajectory.items()}
 
     def compute_discounted_return(self, rewards):
         discounted_return = np.zeros(self.epoch_length, np.float32)
@@ -29,19 +30,23 @@ class Reinforce(RLAlgorithm):
             discounted_return[i] = sum_reward
         return discounted_return
 
+    def train(self):
+        trajectory = self.sample_trajectory()
+        discounted_return = self.compute_discounted_return(trajectory["reward"])
+        discounted_return = standardize_array(discounted_return)
+        args = (self.sess, trajectory["state"], trajectory["action"])
+        self.mean_policy_loss = np.mean([self.policy.update(*args, targets=discounted_return)
+                                         for _ in range(self.num_train)])
+
     def hook_after_step(self, **kwargs):
         super().hook_after_step(**kwargs)
         if self.training:
             state, action, reward, next_state, done = self.transition
-            self.trajectory.append((state, action, reward))
+            self.add_transition(state=state, action=action, reward=reward)
 
     def hook_after_epoch(self, **kwargs):
         super().hook_after_epoch(**kwargs)
         if self.training:
-            states, actions, rewards = self.sample_trajectory()
-            discounted_return = self.compute_discounted_return(rewards)
-            discounted_return = standardize_array(discounted_return)
-            self.mean_policy_loss = np.mean([self.policy.update(self.sess, states, actions, targets=discounted_return)
-                                             for _ in range(self.num_train)])
+            self.train()
             self.add_summaries()
             self.trajectory.clear()
