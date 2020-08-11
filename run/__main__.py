7e284acf94fb7e5654b59d910479f51595e989a9
Raj Kumar Rana <rgk.rana@gmail.com>
2020-08-11 18:56:43 +0200

    Goal trial and reward information are extracted from a csv file based on the environment name

--- run/__main__.py
+++ run/__main__.py
@@ -1,10 +1,11 @@
 import os
 import gym
+from pandas import read_csv
 from datetime import datetime
 import tensorflow.compat.v1 as tf_v1
 from tensorflow.python.util import deprecation
 # Custom modules
-from src.utils import parse_args, random_seed_gen
+from src.utils import parse_args, random_seed_gen, get_reward_info
 from src.config import get_configuration, get_algorithm_from_variant, get_buffer_from_variant, get_policy_from_variant
 
 deprecation._PRINT_DEPRECATION_WARNINGS = False
@@ -13,7 +14,7 @@ TF_CONFIG = tf_v1.ConfigProto(gpu_options=tf_v1.GPUOptions(per_process_gpu_memor
                               allow_soft_placement=True)
 
 
-def run(env, seed, model_i, *, summary_dir, cmd_args, config, sess_config=None):
+def run(env, seed, model_i, *, summary_dir, cmd_args, goal_info, config, sess_config=None):
     """
     Runs the simulation with given parameters
     args:
@@ -32,7 +33,7 @@ def run(env, seed, model_i, *, summary_dir, cmd_args, config, sess_config=None):
     with tf_v1.Session(config=sess_config) as sess:
         buffer = get_buffer_from_variant(config)
         policy = get_policy_from_variant(env, config)
-        algo = get_algorithm_from_variant(sess, env, policy, buffer, model_summary_dir, training, config)
+        algo = get_algorithm_from_variant(sess, env, policy, buffer, model_summary_dir, training, *goal_info, config)
         sess.run(tf_v1.global_variables_initializer())
         if training:
             print("\n# Training: {}".format(model_name))
@@ -53,6 +54,7 @@ def run(env, seed, model_i, *, summary_dir, cmd_args, config, sess_config=None):
 
 
 if __name__ == "__main__":
+    env_info_file = f"./assets/env_info.csv"
     date_time = datetime.now().strftime("%d.%m.%Y %H.%M")
     cmd_args = parse_args()
     summary_dir = os.path.join("summaries", f"{cmd_args.env_name}-{cmd_args.algorithm}-{cmd_args.policy}-{date_time}")
@@ -63,7 +65,10 @@ if __name__ == "__main__":
                                    video_callable=lambda epoch: (not epoch % cmd_args.record_interval) or
                                                                 (epoch == cmd_args.epochs-1))
     config_dict = get_configuration(cmd_args)
+    df = read_csv(env_info_file)
+    goal_info = get_reward_info(df, env_name=cmd_args.env_name)
+    del df
     # Run model
     for model_i, seed in enumerate(random_seed_gen(cmd_args.seed_num), start=1):
         run(seed=seed, model_i=model_i, cmd_args=cmd_args, sess_config=TF_CONFIG, env=env,
-            summary_dir=summary_dir, config=config_dict)
+            summary_dir=summary_dir, goal_info=goal_info, config=config_dict)
