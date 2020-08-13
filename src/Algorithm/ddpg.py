73bc86fc422cb393264018e2d85dbf3a0d454402
Raj Kumar Rana <rgk.rana@gmail.com>
2020-08-14 01:25:07 +0200

    - Inherits OffPolicyAlgorithm - Action randomization migrated from policy.action() to self.action() method

--- src/Algorithm/ddpg.py
+++ src/Algorithm/ddpg.py
@@ -1,17 +1,18 @@
 import numpy as np
 import tensorflow.compat.v1 as tf_v1
-from .rl_algorithm import RLAlgorithm
+from . import OffPolicyAlgorithm
 from src.Layer import QNetwork
 
 
-class DDPG(RLAlgorithm):
+class DDPG(OffPolicyAlgorithm):
     VALID_POLICIES = ["ContinuousPolicy"]
 
-    def __init__(self, *, lr, gamma, tau, update_interval, layer_units, **kwargs):
+    def __init__(self, *, lr, gamma, tau, update_interval, layer_units, sigma, **kwargs):
         super(DDPG, self).__init__(**kwargs)
         self.lr = lr
         self.tau = tau
         self.gamma = gamma
+        self.sigma = sigma
         self.update_interval = update_interval
         # Build networks
         self.target_actor = self.policy.build_network(scope=f"{self.policy.scope}_target")
@@ -25,25 +26,32 @@ class DDPG(RLAlgorithm):
         self.init_actor_loss()
 
     def init_actor_loss(self):
-        loss = -self.critic(tf_v1.concat([self.policy.network.input, self.policy.network.output], axis=-1))
+        states_ph = self.policy.network.input
+        actions_ph = self.policy.network.output
+        loss = -self.critic(tf_v1.concat([states_ph, actions_ph], axis=-1))
         self.policy.set_loss(loss)
 
     def train_actor(self, sess, states):
         feed_dict = {self.policy.network.input: states}
         self.policy.update(sess, feed_dict=feed_dict)
 
+    def action(self, states, **kwargs):
+        action = super().action(states, **kwargs)
+        action += self.sigma * np.random.uniform(-1, 1, size=action.shape)
+        return action
+
     def train_critic(self, sess, states, actions, rewards, next_states, dones):
         # Predict Q-values for next states
-        next_actions = self.action(sess, next_states)
+        next_actions = self.action(next_states)
         next_qs = self.target_critic.predict(sess, np.concatenate([next_states, next_actions], axis=-1))
         q_targets = self.lr * (rewards + self.gamma * (1 - dones) * next_qs.squeeze())
         q_targets = q_targets.reshape((*q_targets.shape, 1))
         self.critic.update(sess, np.concatenate([states, actions], axis=-1), q_targets)
 
-    def train(self, sess):
+    def train(self):
         states, actions, rewards, next_states, dones = self.replay_buffer.sample(self.batch_size)
-        self.train_critic(sess, states, actions, rewards, next_states, dones)
-        self.train_actor(sess, states)
+        self.train_critic(self.sess, states, actions, rewards, next_states, dones)
+        self.train_actor(self.sess, states)
 
     def hook_before_train(self, **kwargs):
         super().hook_before_train(**kwargs)
@@ -54,7 +62,7 @@ class DDPG(RLAlgorithm):
         super(DDPG, self).hook_after_step(**kwargs)
         if self.training:
             self.replay_buffer.add(self.transition)
-            self.train(self.sess)
+            self.train()
             if not self.steps % self.update_interval:
                 self.target_actor.update_weights(self.sess, tau=self.tau)
                 self.target_critic.update_weights(self.sess, tau=self.tau)
@@ -62,5 +70,5 @@ class DDPG(RLAlgorithm):
     def hook_after_epoch(self, **kwargs):
         super().hook_after_epoch(**kwargs)
         if self.training:
-            self.policy.hook_after_action(epoch=self.epoch, **kwargs)
+            self.policy.hook_after_epoch(**kwargs)
             self.add_summaries()
