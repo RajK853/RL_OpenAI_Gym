ea6e1d07476fa176ae72d0dbd60bea92c368c258
Raj Kumar Rana <rgk.rana@gmail.com>
2020-08-14 01:27:38 +0200

    Uses multiple inheritance: OnPolicyAlgorithm as it is online policy and DQN as it is quite similar to DQN algorithm.

--- src/Algorithm/sarsa.py
+++ src/Algorithm/sarsa.py
@@ -1,45 +1,36 @@
 import numpy as np
+from . import OnPolicyAlgorithm
 from .dqn import DQN
 
 
-class Sarsa(DQN):
+class Sarsa(DQN, OnPolicyAlgorithm):
 
     def __init__(self, **kwargs):
         super(Sarsa, self).__init__(**kwargs)
+        self.field_names = ("state", "action", "reward", "next_state", "done")
         self.trajectory = {}
 
-    def add_transition(self, **kwargs):
-        for key, value in kwargs.items():
-            if key not in self.trajectory.keys():
-                self.trajectory[key] = []
-            self.trajectory[key].append(value)
-
-    def sample_trajectory(self):
-        return {key: np.array(value) for key, value in self.trajectory.items()}
-
-    def train(self, sess):
-        trajectory = self.sample_trajectory()
-        states = trajectory["state"]
-        actions = trajectory["action"]
-        rewards = trajectory["reward"]
-        next_states = trajectory["next_state"]
-        dones = trajectory["done"]
+    def train(self):
+        states, actions, rewards, next_states, dones = self.sample_trajectory()
         # Predict Q-values for next states
-        current_qs = self.target_q.predict(sess, states)
-        next_qs = self.target_q.predict(sess, next_states)
-        next_actions = self.action(sess, next_states)
+        current_qs = self.target_q.predict(self.sess, states)
+        next_qs = self.target_q.predict(self.sess, next_states)
+        next_actions = self.action(next_states)
         next_qs = next_qs[np.arange(len(next_actions)), next_actions]
         q_targets = current_qs
         q_targets[np.arange(len(actions)), actions] = self.lr*(rewards + self.gamma*(1 - dones)*next_qs)
-        self.mean_estimator_loss = self.q_net.update(sess, states, q_targets)
+        self.mean_estimator_loss = self.q_net.update(self.sess, states, q_targets)
 
     def hook_after_step(self, **kwargs):
+        OnPolicyAlgorithm.hook_after_step(self, **kwargs)
         if self.training:
             state, action, reward, next_state, done = self.transition
             self.add_transition(state=state, action=action, reward=reward, next_state=next_state, done=done)
-            self.train(self.sess)
 
     def hook_after_epoch(self, **kwargs):
-        super().hook_after_epoch(**kwargs)
+        OnPolicyAlgorithm.hook_after_epoch(self, **kwargs)
         if self.training:
-            self.trajectory.clear()
+            self.policy.hook_after_epoch(epoch=self.epoch)
+            self.train()
+            self.add_summaries()
+            self.clear_trajectory()
