93f399e4ccc4e1aca7c3cb6f46601a7b95016da6
Raj K <rgk.rana@gmail.com>
2021-02-05 20:05:39 +0100

    Added configurations for SAC algorithm

--- src/config.py
+++ src/config.py
@@ -1,17 +1,14 @@
 import copy
 import tensorflow.compat.v1 as tf_v1
 from tensorflow.keras.regularizers import l2
+from tensorflow.keras.constraints import max_norm
 
 from src.Policy import GreedyEpsilonPolicy, GaussianPolicy, DiscretePolicy, ContinuousPolicy
-from src.Algorithm import DQN, DDQN, Reinforce, ActorCritic, Sarsa, DDPG
+from src.Algorithm import DQN, DDQN, Reinforce, ActorCritic, Sarsa, DDPG, SAC
 from src.Buffer import ReplayBuffer, PrioritizedReplayBuffer
 
-ALGO_LEARNING_RATE = 1e-4
-POLICY_LEARNING_RATE = 5e-4
-
-BASE_CONFIG = {
-    "batch_size": 100,
-}
+ALGO_LEARNING_RATE = 3e-4
+POLICY_LEARNING_RATE = 1e-4
 
 _ReplayBuffer = {
     "function": ReplayBuffer,
@@ -47,11 +44,15 @@ Gaussian = {
     "function": GaussianPolicy,
     "kwargs": {
         "alpha": 2e-4,
-        "lr": POLICY_LEARNING_RATE,
-        "layer_units": (100, 100),
+        "lr": 0.1*POLICY_LEARNING_RATE,
+        "shift_scale": 1.5,
+        "min_log_scale": -10,
+        "max_log_scale": 0.69,          # auto = size of action space
+        "layer_units": (256, 256),
         "layer_kwargs": {
             "activation": tf_v1.nn.relu,
-            "kernel_regularizer": l2(1e-6),
+            "kernel_regularizer": None, # l2(1e-6),
+            "kernel_constraint": max_norm(2.0),
         }
     }
 }
@@ -72,7 +73,7 @@ Continuous = {
     "function": ContinuousPolicy,
     "kwargs": {
         "lr": POLICY_LEARNING_RATE,
-        "layer_units": (100, 100, 50),
+        "layer_units": (50, 100, 50),
         "layer_kwargs": {
             "activation": tf_v1.nn.relu,
             "kernel_regularizer": l2(1e-6),
@@ -96,7 +97,12 @@ _DQN = {
     "kwargs": {
         "lr": 0.989,
         "gamma": 0.996,
+        "batch_size": 100,
         "layer_units": (50, 50, 50),
+        "layer_kwargs": {
+            "activation": tf_v1.nn.relu,
+            "kernel_regularizer": l2(1e-6),
+        }
     }
 }
 
@@ -112,21 +118,35 @@ _Sarsa = {
 _DDQN = {
     "function": DDQN,
     "kwargs": {
-        **_DQN["kwargs"],
+        "lr": 0.99,
+        "gamma": 0.996,
+        "layer_units": (50, 50, 25),
         "tau": 0.01,
         "update_interval": 1,
+        "layer_kwargs": {
+            "activation": tf_v1.nn.relu,
+            "kernel_regularizer": l2(1e-6),
+        }
     }
 }
 
 _DDPG = {
     "function": DDPG,
     "kwargs": {
-        "lr": 0.996,
-        "gamma": 0.996,
+        "buffer_size": 500_000,
+        "reward_scale": 1.0,
+        "gamma": 0.99,
+        "batch_size": 100,
         "layer_units": (100, 100, 50),
-        "tau": 0.1,
+        "q_lr": POLICY_LEARNING_RATE,
+        "tau": 0.008,
         "update_interval": 1,
-        "sigma": 0.01,           # Noise value = sigma * random.uniform(-1, 1)
+        "random_type": "normal",
+        "sigma": 0.3,           # Noise value = sigma * random.uniform(-1, 1) or random.normal(loc=0.0, scale=sigma)
+        "layer_kwargs": {
+            "activation": tf_v1.nn.relu,
+            "kernel_regularizer": l2(1e-6),
+        }
     }
 }
 
@@ -141,7 +161,33 @@ _Reinforce = {
 _ActorCritic = {
     "function": ActorCritic,
     "kwargs": {
-        **_Reinforce["kwargs"]
+        "gamma": 0.9,
+        "num_train": 2,
+    }
+}
+
+_SAC = {
+    "function": SAC,
+    "kwargs": {
+        "buffer_size": 500_000,
+        "reward_scale": 1.0,
+        "gamma": 0.99,
+        "batch_size": 256,
+        "num_init_exp_samples": 10000,
+        "layer_units": (100, 100),
+        "tau": 5e-3,            # param(targets) = tau*param(source) + (1-tau)*param(targets) 
+        "update_interval": 1,   # Time steps
+        "num_q_nets": 2,
+        "q_lr": POLICY_LEARNING_RATE,
+        "alpha_lr": POLICY_LEARNING_RATE,
+        "auto_ent": True,
+        "target_entropy": -2,    # auto = -0.5 * Action space size
+        "init_log_alpha": 0.0,   # auto = 2 * Target entropy
+        "layer_kwargs": {
+            "activation": tf_v1.nn.relu,
+            "kernel_regularizer": None # l2(1e-6),
+            # "kernel_constraint": max_norm(2.0),
+        }
     }
 }
 
@@ -152,17 +198,15 @@ ALGORITHMS = {
     "actor_critic": _ActorCritic,
     "sarsa": _Sarsa,
     "ddpg": _DDPG,
+    "sac": _SAC,
 }
 
 
 def get_configuration(args):
-    replay_buffer = copy.deepcopy(REPLAY_BUFFERS[args.buffer])
     policy = copy.deepcopy(POLICIES[args.policy])
     algorithm = copy.deepcopy(ALGORITHMS[args.algorithm])
-    algorithm["kwargs"].update(render=args.render,
-                               **copy.deepcopy(BASE_CONFIG))
+    algorithm["kwargs"].update(render=args.render)
     config_dict = {
-        # "buffer_param": replay_buffer,
         "policy_param": policy,
         "algorithm_param": algorithm,
     }
@@ -185,8 +229,8 @@ def get_policy_from_variant(env, variant):
     return func(env=env, **kwargs)
 
 
-def get_algorithm_from_variant(sess, env, policy, summary_dir, training, goal_trials, goal_reward, variant):
+def get_algorithm_from_variant(sess, env, policy, summary_dir, training, load_model, goal_trials, goal_reward, variant):
     param = variant["algorithm_param"]
     func, kwargs = get_func_and_kwargs_from_param(param)
     return func(sess=sess, env=env, policy=policy, summary_dir=summary_dir,
-                training=training, goal_trials=goal_trials, goal_reward=goal_reward, **kwargs)
+                training=training, goal_trials=goal_trials, goal_reward=goal_reward, load_model=load_model, **kwargs)
