bf54a0e4a75385e79d2caabab1196033d6068fdc
Raj K <rgk.rana@gmail.com>
2021-03-28 00:45:44 +0100

    - Added default neural network architecture - NN learning rate uses the Scheduler module

--- src/Policy/continuous.py
+++ src/Policy/continuous.py
@@ -1,45 +1,73 @@
 import tensorflow as tf
 import tensorflow.compat.v1 as tf_v1
 
+from src.utils import get_scheduler
 from .base_policy import BasePolicy
-from src.Layer import NeuralNetwork
+from src.Network.neural_network import NeuralNetwork
+
+l2 = tf_v1.keras.regularizers.l2
+DEFAULT_LAYERS = [
+    {"type": "Dense", "units": 256, "activation": "relu", "kernel_regularizer": l2(1e-8)},
+    {"type": "LayerNormalization"},
+    {"type": "Dense", "units": 256, "activation": "relu", "kernel_regularizer": l2(1e-8)},
+    {"type": "Dense", "units": 1, "activation": "tanh"},
+]
 
 
 class ContinuousPolicy(BasePolicy):
 
-    def __init__(self, *, layer_units, lr=1e-3, **kwargs):
+    def __init__(self, *, lr_kwargs, layers=None, **kwargs):
         self.scope = self.__class__.__name__
-        super(ContinuousPolicy, self).__init__(env=kwargs.pop("env"))
-        self.lr = lr
-        self.layer_units = layer_units
-        self.network_kwargs = kwargs
-        self.network = self.build_network(self.scope)
+        super(ContinuousPolicy, self).__init__(**kwargs)
+        self.lr_scheduler = get_scheduler(lr_kwargs)
+        self.lr_ph = tf_v1.placeholder("float32", shape=[], name=f"{self.scope}/lr_ph")
+        if layers is None:
+            layers = DEFAULT_LAYERS
+        layers[-1]["units"] = self.action_size
+        self.layers = layers
+        self.model = NeuralNetwork(self.scope, input_shape=self.obs_shape, layers=self.layers)
         self._loss = None
         self.train_op = None
         # Summary parameters
-        self.scalar_summaries += ("loss", )
+        self.scalar_summaries_tf += ("loss", )
+        self.scalar_summaries += ("lr", )
 
-    def build_network(self, scope):
-        network = NeuralNetwork(scope, input_shape=self.obs_shape, layer_units=self.layer_units,
-                                output_size=self.action_size, **self.network_kwargs)
-        return network
+    @property
+    def lr(self):
+        return self.lr_scheduler.value
 
-    def set_loss(self, loss, optimizer=None):
+    @property
+    def input(self):
+        return self.model.input
+
+    @property
+    def output(self):
+        return self.model.output
+
+    def setup_loss(self, loss, train_op):
         self._loss = loss
-        optimizer = tf_v1.train.AdamOptimizer(learning_rate=self.lr) if optimizer is None else optimizer
-        self.train_op = optimizer.minimize(self._loss, var_list=self.network.trainable_vars)
+        self.train_op = train_op
+
+    @property
+    def trainable_vars(self):
+        return self.model.trainable_vars
 
     @property
     def loss(self):
         return tf.reduce_mean(self._loss)
 
-    def _action(self, sess, states, network=None, **kwargs):
-        if network is None:
-            network = self.network
-        actions = sess.run(self.network.output, feed_dict={network.inputs_ph: states})
+    def _action(self, sess, states, **kwargs):
+        actions = sess.run(self.output, feed_dict={self.input: states})
         return actions
 
-    def update(self, sess, feed_dict=None, **kwargs):
-        assert isinstance(feed_dict, dict), f"Invalid value received in feed_dict; {feed_dict}"
-        _, loss, self.summary = sess.run([self.train_op, self._loss, self.summary_op], feed_dict=feed_dict)
-        return loss
+    def update(self, sess, feed_dict, **kwargs):
+        train_ops = [self.train_op, self._loss]
+        if self.summary_op is not None:
+            train_ops.append(self.summary_op)
+        results = sess.run(train_ops, feed_dict={self.lr_ph: self.lr, **feed_dict})
+        if self.summary_op is not None:
+            self.summary = results[-1]
+
+    def hook_after_epoch(self, **kwargs):
+        super().hook_after_epoch(**kwargs)
+        self.lr_scheduler.increment()
