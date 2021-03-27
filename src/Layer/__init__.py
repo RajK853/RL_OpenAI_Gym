28147069fa9596433d88d11e2d2017fd6f8fa911
Raj K <rgk.rana@gmail.com>
2021-03-28 00:33:20 +0100

    Removed Layer module

--- src/Layer/__init__.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-ROOT_DIR = os.path.dirname(os.path.abspath(os.path.join(os.path.abspath(""), "..")))
-sys.path.insert(0, ROOT_DIR)
-
-from .neural_network import NeuralNetwork
-from .qnetwork import QNetwork
