6aaf8a17cdb4056890ea0483f21424366f2c9069
Raj K <rgk.rana@gmail.com>
2021-03-28 00:49:06 +0100

    - Added default neural network architecture - NN learning rate uses the Scheduler module - Clipped both mean and log_std from the NN - Uses MultiVariateNormalDistribution and Bijectors to generate actions and their log probabilites

--- src/Policy/gaussian.py
+++ src/Policy/gaussian.py
@@ -1,148 +1,174 @@
 import tensorflow.compat.v1 as tf_v1
 import tensorflow_probability as tfp
 
-from src.Layer import NeuralNetwork
+from src.Network.neural_network import NeuralNetwork
+from src.Network.utils import get_clipped_train_op
 from .base_policy import BasePolicy
-import numpy as np
+from src import Scheduler
 
 tfd = tfp.distributions
 tfb = tfp.bijectors
 
+l2 = tf_v1.keras.regularizers.l2
+DEFAULT_LAYERS = [
+    {"type": "Dense", "units": 256, "activation": "relu", "kernel_regularizer": l2(1e-8)},
+    {"type": "LayerNormalization"},
+    {"type": "Dense", "units": 256, "activation": "relu", "kernel_regularizer": l2(1e-8)},
+    {"type": "Dense", "units": 1}
+]
+
 
 class SquashBijector(tfp.bijectors.Bijector):
     """
-    This squash bijector is derived from here:
+    This squash bijector is derived from the given source:
     https://github.com/avisingh599/reward-learning-rl/blob/master/softlearning/distributions/squash_bijector.py
     """
     def __init__(self, validate_args=False, name="tanh"):
         super(SquashBijector, self).__init__(forward_min_event_ndims=0, validate_args=validate_args, name=name)
 
     def _forward(self, x):
-        return tf_v1.nn.tanh(x)
+        return tf_v1.tanh(x)
 
     def _inverse(self, y):
         return tf_v1.atanh(y)
 
     def _forward_log_det_jacobian(self, x):
-        return 2. * (np.log(2.) - x - tf_v1.nn.softplus(-2. * x))
+        return 2.0*(tf_v1.log(2.0) - x - tf_v1.nn.softplus(-2.0*x))
 
 
 class GaussianPolicy(BasePolicy):
 
-    def __init__(self, *, layer_units, alpha=1e-3, lr=1e-3, shift_scale=1.5,
-                 min_log_scale=-20, max_log_scale=2, **kwargs):
-        super(GaussianPolicy, self).__init__(env=kwargs.pop("env"))
-        self.lr = lr
+    def __init__(self, *, lr_kwargs, layers=None, alpha=1e-3, learn_std=False, std_value=0.1, mu_range=None,
+                 log_std_range=None, **kwargs):
+        super(GaussianPolicy, self).__init__(**kwargs)
+        scheduler = getattr(Scheduler, lr_kwargs.pop("type"))
+        self.lr_scheduler = scheduler(**lr_kwargs)
         self.alpha = alpha
-        self.eps = np.finfo(np.float32).eps.item()
-        self.shift_scale = shift_scale
+        self.mu_range = (-2.0, 2.0) if mu_range is None else mu_range
+        self.log_std_range = (-20, 0.1) if log_std_range is None else log_std_range
         assert not self.discrete_action_space, "Action space for the Gaussian Policy must be continuous!"
-        self.actions_ph = tf_v1.placeholder(tf_v1.float32, shape=(None, *self.action_shape), name="action_ph")
-        self.targets_ph = tf_v1.placeholder(tf_v1.float32, shape=(None,), name="target_ph")
-        with tf_v1.variable_scope(self.scope, reuse=tf_v1.AUTO_REUSE):
-            self.network = NeuralNetwork(scope="neural_network", input_shape=self.obs_shape, layer_units=layer_units,
-                                         output_size=2*self.action_size, **kwargs)
-            self.shift = self.network.output[:, :self.action_size]
-            # Limit shift in the range (-shift_scale, shift_scale) as large shift values produce NaNs in action
-            self.shift = shift_scale*tf_v1.math.sin(self.shift)         
-            self.log_scale = self.network.output[:, self.action_size:]
-            self.log_scale = tf_v1.clip_by_value(self.log_scale, min_log_scale, max_log_scale)
-            self.scale = tf_v1.exp(self.log_scale)
-            batch_size = tf_v1.shape(self.shift)[0]
-            norm_dist = tfd.MultivariateNormalDiag(loc=tf_v1.zeros(self.action_size),
-                                                   scale_diag=tf_v1.ones(self.action_size))
-            bijector = tfb.Chain([SquashBijector(),
-                                  tfb.Affine(shift=self.shift, scale_diag=self.scale)])
-            self.dist = tfd.TransformedDistribution(distribution=norm_dist, bijector=bijector)
-            self.actions = self.dist.sample(batch_size)
-            self.deterministic_actions = self.clip_action(self.shift, name="det_actions")
-            self.log_actions = self.log_prob(self.actions_ph)
+        # Placeholders
+        self.lr_ph = tf_v1.placeholder("float32", shape=(), name="lr_ph")
+        self.targets_ph = tf_v1.placeholder("float32", shape=(None, 1), name="target_ph")
+        # Create model
+        if layers is None:
+            layers = DEFAULT_LAYERS
+        # layers[-1]["units"] = 2*self.action_size
+        self.layers = layers
+        self.base_model = NeuralNetwork(self.scope, input_shape=self.obs_shape, layers=self.layers)
+        self.mu = tf_v1.keras.layers.Dense(self.action_size, activation=None, name="mu")(self.base_model.output)
+        self.mu = tf_v1.clip_by_value(self.mu, *self.mu_range)
+        if learn_std:
+            self.log_std = tf_v1.keras.layers.Dense(self.action_size, name="log_std")(self.base_model.output)
+            self.log_std = tf_v1.clip_by_value(self.log_std, *self.log_std_range)
+            self.std = tf_v1.exp(self.log_std)
+        else:
+            self.std = tf_v1.constant([std_value]*self.action_size, dtype="float32")
+        self.raw_action_model = tf_v1.keras.Model(inputs=[self.base_model.input], outputs=[self.mu, self.std])
+        norm_dist = tfd.MultivariateNormalDiag(loc=tf_v1.zeros(self.action_size),
+                                               scale_diag=tf_v1.ones(self.action_size))
+        norm_dist = tfd.Independent(norm_dist)
+        batch_size = tf_v1.shape(self.mu)[0]
+        latents = norm_dist.sample(batch_size)
+        bijector = tfb.Chain([SquashBijector(), tfb.Affine(shift=self.mu, scale_diag=self.std)])
+        self.actions = bijector.forward(latents)
+        self.deterministic_actions = tf_v1.tanh(self.mu)
+        self.model = tf_v1.keras.Model(inputs=[self.base_model.input], outputs=[self.actions])
         # Loss parameters
         self._loss = None
         self.train_op = None
         # Summary parameters
-        self.scalar_summaries += ("mean_loss", "mean_entropy", "min_shift", "mean_shift", "max_shift",
-                                  "min_scale", "mean_scale", "max_scale")
-        self.histogram_summaries += ("log_pi",)
+        self.scalar_summaries += ("lr", )
+        self.scalar_summaries_tf += ("loss", "mean_entropy", "min_mu", "mean_mu", "max_mu", "min_std", "mean_std",
+                                     "max_std")
+        self.histogram_summaries_tf += ("log_actions", "actions", "mu", "std")
 
-    def clip_action(self, action, **kwargs):
-        return tf_v1.tanh(action)
-
-    def log_prob(self, action, **kwargs):
-        return self.dist.log_prob(action, **kwargs)
-
-    @property
-    def log_pi(self):
-        return self.log_prob(self.actions)
-
-    @property
-    def mean_shift(self):
-        return tf_v1.reduce_mean(self.shift)
+    def log_prob(self, state, action, **kwargs):
+        mu, std = self.raw_action_model(state)
+        norm_dist = tfd.MultivariateNormalDiag(loc=tf_v1.zeros(self.action_size),
+                                               scale_diag=tf_v1.ones(self.action_size))
+        bijector = tfb.Chain([SquashBijector(), tfb.Affine(shift=mu, scale_diag=std)])
+        dist = tfd.TransformedDistribution(distribution=norm_dist, bijector=bijector)
+        log_pis = dist.log_prob(action)
+        return log_pis
 
     @property
-    def min_shift(self):
-        return tf_v1.reduce_min(self.shift)
+    def log_actions(self):
+        return self.log_prob(self.base_model.input, self.actions)
 
     @property
-    def max_shift(self):
-        return tf_v1.reduce_max(self.shift)
+    def lr(self):
+        return self.lr_scheduler.value
+
+    @property
+    def mean_mu(self):
+        return tf_v1.reduce_mean(self.mu)
+
+    @property
+    def min_mu(self):
+        return tf_v1.reduce_min(self.mu)
+
+    @property
+    def max_mu(self):
+        return tf_v1.reduce_max(self.mu)
     
     @property
-    def mean_scale(self):
-        return tf_v1.reduce_mean(self.scale)
+    def mean_std(self):
+        return tf_v1.reduce_mean(self.std)
 
     @property
-    def min_scale(self):
-        return tf_v1.reduce_min(self.scale)
+    def min_std(self):
+        return tf_v1.reduce_min(self.std)
 
     @property
-    def max_scale(self):
-        return tf_v1.reduce_max(self.scale)
+    def max_std(self):
+        return tf_v1.reduce_max(self.std)
 
     @property
     def mean_entropy(self):
-        return tf_v1.reduce_mean(-self.log_pi)
+        return tf_v1.reduce_mean(-self.log_actions)
 
     @property
-    def mean_loss(self):
-        return tf_v1.reduce_mean(self.loss)
+    def loss(self):
+        return tf_v1.reduce_mean(self._loss)
 
     @property
     def input(self):
-        return self.network.input
+        return self.model.input
 
     @property
     def output(self):
-        return self.network.output
+        return self.model.output
+
+    @property
+    def trainable_vars(self):
+        return self.model.trainable_variables
 
     def init_default_loss(self):
-        log_loss = self.log_pi * self.targets_ph
-        entropy_loss = -self.alpha * self.dist.entropy()
+        log_loss = -self.log_actions * self.targets_ph
+        entropy_loss = 0.0  # -self.alpha * self.dist.entropy()
         loss = log_loss + entropy_loss
-        self.set_loss(loss=loss)
+        optimizer = tf_v1.train.AdamOptimizer(learning_rate=self.lr_ph)
+        train_op = get_clipped_train_op(loss, optimizer, var_list=self.trainable_vars, clip_norm=self.clip_norm)
+        self.setup_loss(loss, train_op)
 
-    def set_loss(self, loss, train_op=None):
+    def setup_loss(self, loss, train_op):
         self._loss = loss
-        if train_op is None:
-            optimizer = tf_v1.train.AdamOptimizer(learning_rate=self.lr)
-            self.train_op = optimizer.minimize(self._loss, var_list=self.network.trainable_vars)
-        else:
-            self.train_op = train_op
+        self.train_op = train_op
 
     def _action(self, sess, states, deterministic=False, **kwargs):
         action_op = self.deterministic_actions if deterministic else self.actions
-        actions = sess.run(action_op, feed_dict={self.network.inputs_ph: states})
+        actions = sess.run(action_op, feed_dict={self.model.input: states})
         return actions
 
-    def log_action(self, sess, states, actions, **kwargs):
-        log_actions = sess.run(self.log_actions, feed_dict={self.network.inputs_ph: states, self.actions_ph: actions})
-        return log_actions
+    def update(self, sess, feed_dict, **kwargs):
+        train_ops = [self.train_op, self._loss]
+        if self.summary_op is not None:
+            train_ops.append(self.summary_op)
+        results = sess.run(train_ops, feed_dict={self.lr_ph: self.lr, **feed_dict})
+        if self.summary_op is not None:
+            self.summary = results[-1]
 
-    def update(self, sess, states, actions=None, feed_dict=None, **kwargs):
-        _feed_dict = {self.network.inputs_ph: states}
-        if actions is not None:
-            _feed_dict.update({self.actions_ph: actions})
-        if feed_dict is not None:
-            _feed_dict.update(feed_dict)
-        _, loss, self.summary = sess.run([self.train_op, self.loss, self.summary_op], feed_dict=_feed_dict)
-        return loss
+    def hook_after_epoch(self, **kwargs):
+        super().hook_after_epoch(**kwargs)
+        self.lr_scheduler.increment()
