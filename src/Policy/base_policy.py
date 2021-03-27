62d2d9ef51db19d4823c2645ad5eaa0d53e49393
Raj K <rgk.rana@gmail.com>
2021-03-28 00:38:48 +0100

    Added clip_norm attribute

--- src/Policy/base_policy.py
+++ src/Policy/base_policy.py
@@ -1,4 +1,3 @@
-import numpy as np
 import tensorflow.compat.v1 as tf_v1
 from gym.spaces import Discrete
 
@@ -7,19 +6,23 @@ from src.utils import get_space_size
 
 class BasePolicy:
 
-    def __init__(self, *, env, name="policy"):
+    def __init__(self, *, env, name="policy", clip_norm=2.0):
         self.scope = name
+        self.clip_norm = clip_norm
         self.action_space = env.action_space
         self.observation_space = env.observation_space
         self.action_size = get_space_size(self.action_space)
         self.observation_size = get_space_size(self.observation_space)
         self.discrete_action_space = isinstance(self.action_space, Discrete)
         self.scalar_summaries = ()
+        self.scalar_summaries_tf = ()
         self.histogram_summaries = ()
+        self.histogram_summaries_tf = ()
         self.summary_op = None
         self.summary = None
         self._loss = None
         self.train_op = None
+        self._trainable_vars = None
 
     @property
     def loss(self):
@@ -34,33 +37,46 @@ class BasePolicy:
     def obs_shape(self):
         return self.observation_space.shape
 
-    @staticmethod
-    def process_action(actions):
-        actions = np.squeeze(actions)
-        if not isinstance(actions, np.ndarray):
-            return actions.item()
-        return actions
+    @property
+    def trainable_vars(self):
+        if self._trainable_vars is None:
+            self._trainable_vars = tf_v1.get_collection(tf_v1.GraphKeys.TRAINABLE_VARIABLES, scope=self.scope)
+        return self._trainable_vars
+
+    def clip_action(self, action, **kwargs):
+        return tf_v1.clip_by_value(action, self.action_space.low, self.action_space.high, **kwargs)
 
     def reshape_state(self, states):
-        states = states.reshape(-1, *self.observation_space.shape)
+        states = states.reshape(-1, *self.obs_shape)
         return states
 
     def action(self, sess, states, **kwargs):
         states = self.reshape_state(states)
         actions = self._action(sess, states, **kwargs)
-        actions = self.process_action(actions)
         return actions
 
     def _action(self, sess, states, **kwargs):
         raise NotImplementedError
 
-    def update(self, sess, states, actions, **kwargs):
+    def update(self, sess, states, actions, *args, **kwargs):
         return 0.0
+
+    def hook_before_train(self, **kwargs):
+        pass
 
     def hook_before_epoch(self, **kwargs):
         pass
 
+    def hook_before_step(self, **kwargs):
+        pass
+
+    def hook_after_step(self, **kwargs):
+        pass
+
     def hook_after_epoch(self, **kwargs):
+        pass
+
+    def hook_after_train(self, **kwargs):
         pass
 
     def init_summaries(self, tag="", force=False):
@@ -68,7 +84,7 @@ class BasePolicy:
             _summaries = []
             for summary_type in ("scalar", "histogram"):
                 summary_func = getattr(tf_v1.summary, summary_type)
-                for summary_attr in getattr(self, f"{summary_type}_summaries"):
+                for summary_attr in getattr(self, f"{summary_type}_summaries_tf"):
                     attr = getattr(self, summary_attr)
                     _summaries.append(summary_func(f"{tag}/{self.scope}/{summary_attr}", attr))
             if _summaries:
