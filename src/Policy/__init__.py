c1d49408eb802b451db7433ac5d92afdd292395e
Raj K <rgk.rana@gmail.com>
2021-03-28 00:37:34 +0100

    Removed redundant codes

--- src/Policy/__init__.py
+++ src/Policy/__init__.py
@@ -3,12 +3,3 @@ from .discrete import DiscretePolicy
 from .gaussian import GaussianPolicy
 from .greedy_epsilon import GreedyEpsilonPolicy
 from .continuous import ContinuousPolicy
-
-POLICIES = {"greedy_epsilon": GreedyEpsilonPolicy,
-            "gaussian": GaussianPolicy,
-            "discrete": DiscretePolicy,}
-
-
-def get_policy(name, **kwargs):
-    policy_func = POLICIES[name]
-    return policy_func(**kwargs)
