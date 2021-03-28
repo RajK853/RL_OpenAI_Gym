ee5922b54270354e5a4ee44a806c398166a4c18a
Raj K <rgk.rana@gmail.com>
2021-03-28 01:06:19 +0100

    - Learning rate uses the Scheduler module -

--- src/Algorithm/reinforce.py
+++ src/Algorithm/reinforce.py
@@ -1,49 +1,51 @@
 import numpy as np
 from . import OnPolicyAlgorithm
-from src.utils import standardize_array
+from src.utils import get_scheduler, standardize_array
 
 
 class Reinforce(OnPolicyAlgorithm):
     VALID_POLICIES = {"DiscretePolicy", "GaussianPolicy"}
 
-    def __init__(self, *, gamma=0.99, num_train=1, **kwargs):
+    def __init__(self, *, gamma_kwargs, num_train=1, **kwargs):
         super(Reinforce, self).__init__(**kwargs)
-        self.gamma = gamma
-        self.trajectory = {}
+        self.gamma_scheduler = get_scheduler(gamma_kwargs)
         self.num_train = num_train
-        self.mean_policy_loss = 0.0
-        self.field_names = ("state", "action", "reward")
-        self.scalar_summaries += ("mean_policy_loss", )
+        self.field_names = ("state", "action", "reward")    # Used to sample out the trajectory
+        self.scalar_summaries += ("gamma", )
+
+    @property
+    def gamma(self):
+        return self.gamma_scheduler.value
 
     def compute_discounted_return(self, rewards):
-        discounted_return = np.zeros(self.epoch_length, np.float32)
-        sum_reward = 0
+        discounted_return = 0
+        returns = np.zeros(self.epoch_length, np.float32)
         for i in reversed(range(self.epoch_length)):
-            sum_reward = rewards[i] + self.gamma * sum_reward
-            discounted_return[i] = sum_reward
-        return discounted_return
+            discounted_return = rewards[i] + self.gamma * discounted_return
+            returns[i] = discounted_return
+        return returns
 
     def train(self):
         states, actions, rewards = self.sample_trajectory()
         discounted_return = self.compute_discounted_return(rewards)
         discounted_return = standardize_array(discounted_return)
-        args = (self.sess, states, actions)
-        self.mean_policy_loss = np.mean([self.policy.update(*args, targets=discounted_return)
-                                         for _ in range(self.num_train)])
+        discounted_return = np.expand_dims(discounted_return, axis=-1)
+        feed_dict = {self.policy.model.input: states, self.policy.actions_ph: actions,
+                     self.policy.targets_ph: discounted_return}
+        for _ in range(self.num_train):
+            self.policy.update(self.sess, feed_dict=feed_dict)
 
     def hook_before_train(self, **kwargs):
         self.policy.init_default_loss()
         super().hook_before_train(**kwargs)
+
+    def hook_after_epoch(self, **kwargs):
+        super().hook_after_epoch(**kwargs)
+        if self.training:
+            self.gamma_scheduler.increment()
 
     def hook_after_step(self, **kwargs):
         super().hook_after_step(**kwargs)
         if self.training:
             state, action, reward, *_ = self.transition
             self.add_transition(state=state, action=action, reward=reward)
-
-    def hook_after_epoch(self, **kwargs):
-        super().hook_after_epoch(**kwargs)
-        if self.training:
-            self.train()
-            self.add_summaries()
-            self.trajectory.clear()
