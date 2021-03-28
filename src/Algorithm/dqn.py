9bb227ddb98c4f2da9f917f60729b4b3f55d8151
Raj K <rgk.rana@gmail.com>
2021-03-28 01:04:26 +0100

    - Discount rate and learning rate uses the Scheduler module - TD loss implemented using tensors only -

--- src/Algorithm/dqn.py
+++ src/Algorithm/dqn.py
@@ -1,37 +1,75 @@
-import numpy as np
-from src.Layer import QNetwork
+import tensorflow.compat.v1 as tf_v1
+
 from . import OffPolicyAlgorithm
+from src.utils import get_scheduler
+from src.Network import QNetwork
+from src.Network.utils import get_clipped_train_op
 
 
 class DQN(OffPolicyAlgorithm):
     VALID_POLICIES = ["GreedyEpsilonPolicy"]
 
-    def __init__(self, *, gamma=0.9, lr=0.99, layer_units=None, **kwargs):
+    def __init__(self, *, lr_kwargs, gamma_kwargs, reward_scale=1.0, layers=None, **kwargs):
         super(DQN, self).__init__(**kwargs)
-        self.gamma = gamma
-        self.lr = lr
-        self.layer_units = layer_units or (50, 50, 50)
-        self.q_net = QNetwork(self.obs_shape, self.action_size, layer_units=self.layer_units, scope="q_network")
+        self.lr_scheduler = get_scheduler(lr_kwargs)
+        self.gamma_scheduler = get_scheduler(gamma_kwargs)
+        self.reward_scale = reward_scale
+        self.layers = layers
+        self.q_net = QNetwork(input_shape=self.obs_shape, output_size=self.action_size, layers=layers, scope="q_network")
         self.target_q = self.q_net
-        self.mean_estimator_loss = 0.0
-        self.summary_init_objects += (self.q_net,)
-        self.scalar_summaries += ("eps", "mean_estimator_loss")
+        # Placeholders
+        self.lr_ph = tf_v1.placeholder("float32", shape=[], name="lr_ph")
+        self.states_ph = tf_v1.placeholder("float32", shape=[None, *self.obs_shape], name="states_ph")
+        self.actions_ph = tf_v1.placeholder("int32", shape=[None], name="actions_ph")
+        self.rewards_ph = tf_v1.placeholder("float32", shape=[None], name="rewards_ph")
+        self.next_states_ph = tf_v1.placeholder("float32", shape=[None, *self.obs_shape], name="next_states_ph")
+        self.dones_ph = tf_v1.placeholder("float32", shape=[None], name="dones_ph")
+        # Summary ops
+        self.summary_init_objects += (self.q_net, )
+        self.scalar_summaries += ("gamma", "lr")
 
     @property
-    def eps(self):
-        return self.policy.eps
+    def gamma(self):
+        return self.gamma_scheduler.value
 
-    def action(self, states, **kwargs):
-        return super().action(states, estimator=self.q_net, **kwargs)
+    @property
+    def lr(self):
+        return self.lr_scheduler.value
+
+    def action(self, state, **kwargs):
+        return super().action(state, estimator=self.q_net, **kwargs)
+
+    def get_q_target(self):
+        next_qs = self.target_q(self.next_states_ph)
+        max_next_qs = tf_v1.reduce_max(next_qs, axis=-1)
+        q_targets = self.reward_scale*self.rewards_ph + self.gamma*max_next_qs*(1.0 - self.dones_ph)
+        return q_targets
+
+    def init_q(self):
+        q_targets = self.get_q_target()
+        batch_size = tf_v1.shape(self.actions_ph)[0]
+        indices = tf_v1.stack([tf_v1.range(batch_size), self.actions_ph], axis=-1)
+        q_predictions = self.q_net(self.states_ph)
+        q_predictions = tf_v1.gather_nd(q_predictions, indices)
+        q_loss = tf_v1.reduce_mean(tf_v1.losses.mean_squared_error(labels=q_targets, predictions=q_predictions))
+        optimizer = tf_v1.train.AdamOptimizer(learning_rate=self.lr_ph)
+        train_op = get_clipped_train_op(q_loss, optimizer=optimizer, var_list=self.q_net.trainable_vars,
+                                        clip_norm=self.clip_norm)
+        self.q_net.setup_loss(q_loss, train_op)
+
+    def hook_before_train(self, **kwargs):
+        self.init_q()
+        super().hook_before_train(**kwargs)
 
     def train(self):
         states, actions, rewards, next_states, dones = self.replay_buffer.sample(self.batch_size)
-        # Predict Q-values for next states
-        current_qs = self.target_q.predict(self.sess, states)
-        next_qs = self.target_q.predict(self.sess, next_states)
-        q_targets = current_qs
-        q_targets[np.arange(len(actions)), actions] = self.lr*(rewards + self.gamma*(1 - dones)*np.amax(next_qs, axis=-1))
-        self.mean_estimator_loss = self.q_net.update(self.sess, states, q_targets)
+        feed_dict = {self.states_ph: states,
+                     self.actions_ph: actions,
+                     self.rewards_ph: rewards,
+                     self.next_states_ph: next_states,
+                     self.dones_ph: dones,
+                     self.lr_ph: self.lr}
+        self.q_net.update(self.sess, feed_dict)
 
     def hook_after_step(self, **kwargs):
         if self.training:
@@ -41,5 +79,6 @@ class DQN(OffPolicyAlgorithm):
     def hook_after_epoch(self, **kwargs):
         super().hook_after_epoch(**kwargs)
         if self.training:
-            self.policy.hook_after_epoch(epoch=self.epoch, **kwargs)
-            self.add_summaries()
+            self.add_summaries(self.epoch)
+            self.gamma_scheduler.increment()
+            self.lr_scheduler.increment()
