14c77d758bd73750e77845ffd03f63c425547886
Raj K <rgk.rana@gmail.com>
2021-03-28 01:07:50 +0100

    Minor changes

--- src/Algorithm/actor_critic.py
+++ src/Algorithm/actor_critic.py
@@ -1,42 +1,65 @@
 import numpy as np
+import tensorflow.compat.v1 as tf_v1
+
 from . import Reinforce
-from src.Layer import QNetwork
+from src.utils import get_scheduler
+from src.Network.qnetwork import QNetwork
+from src.Network.utils import get_clipped_train_op
 
 
 class ActorCritic(Reinforce):
 
-    def __init__(self, **kwargs):
+    def __init__(self, *, lr_kwargs, baseline_scale=0.1, layers=None, **kwargs):
         super(ActorCritic, self).__init__(**kwargs)
-        self.critic = QNetwork(self.obs_shape, output_size=1, layer_units=(50, 50), scope="critic")
-        self.mean_critic_loss = None
+        self.baseline_scale = baseline_scale
+        self.critic = QNetwork(input_shape=self.obs_shape, output_size=1, layers=layers, scope="critic")
+        self.lr_scheduler = get_scheduler(lr_kwargs)
+        # Placeholders
+        self.lr_ph = tf_v1.placeholder("float32", shape=(), name="lr_ph")
+        self.targets_ph = tf_v1.placeholder("float32", shape=(None, 1), name="targets_ph")
         self.summary_init_objects += (self.critic, )
-        self.scalar_summaries += ("mean_critic_loss", )
+        self.scalar_summaries += ("lr", )
 
-    def calculate_advantage(self, states, rewards):
-        discounted_return = self.compute_discounted_return(rewards)
-        values = self.critic.predict(sess=self.sess, inputs=states)
-        advantage = discounted_return - values.squeeze()
-        return advantage
+    @property
+    def lr(self):
+        return self.lr_scheduler.value
+
+    def init_critic(self):
+        loss = tf_v1.losses.mean_squared_error(labels=self.targets_ph, predictions=self.critic.output)
+        optimizer = tf_v1.train.AdamOptimizer(learning_rate=self.lr_ph)
+        train_op = get_clipped_train_op(loss, optimizer=optimizer, var_list=self.critic.trainable_vars,
+                                        clip_norm=self.clip_norm)
+        self.critic.setup_loss(loss, train_op)
 
     def train_actor(self, states, actions, advantage):
-        self.mean_policy_loss = np.mean([self.policy.update(self.sess, states, actions, targets=advantage)
-                                         for _ in range(self.num_train)])
+        feed_dict = {
+            self.policy.model.input: states,
+            self.policy.actions_ph: actions,
+            self.policy.targets_ph: advantage}
+        self.policy.update(self.sess, feed_dict=feed_dict)
 
-    def train_critic(self, states, advantage):
-        # TODO: Refactor this to not use reshape
-        advantage = advantage.reshape((*advantage.shape, 1))
-        self.mean_critic_loss = np.mean([self.critic.update(self.sess, states, targets=advantage)
-                                         for _ in range(self.num_train)])
+    def train_critic(self, states, targets):
+        feed_dict = {
+            self.critic.input: states,
+            self.targets_ph: targets,
+            self.lr_ph: self.lr}
+        self.critic.update(self.sess, feed_dict=feed_dict)
 
     def train(self):
         states, actions, rewards = self.sample_trajectory()
-        advantage = self.calculate_advantage(states, rewards)
-        self.train_actor(states, actions, advantage)
-        self.train_critic(states, advantage)
+        discounted_return = self.compute_discounted_return(rewards)
+        # discounted_return = standardize_array(discounted_return)
+        discounted_return = np.expand_dims(discounted_return, axis=-1)
+        values = self.critic.predict(states)
+        advantage = discounted_return - values
+        for _ in range(self.num_train):
+            self.train_critic(states, advantage)
+            self.train_actor(states, actions, advantage)
 
-    """def hook_after_epoch(self, **kwargs):
-        super(Reinforce, self).hook_after_epoch(**kwargs)
-        if self.training:
-            self.train()
-            self.add_summaries()
-            self.trajectory.clear()"""
+    def hook_before_train(self, **kwargs):
+        self.init_critic()
+        super().hook_before_train(**kwargs)
+
+    def hook_after_epoch(self, **kwargs):
+        super().hook_after_epoch(**kwargs)
+        self.lr_scheduler.increment()
