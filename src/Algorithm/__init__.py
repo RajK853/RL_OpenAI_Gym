8b7653f4de3685a21d54eff78f8d13892e4435b3
Raj Kumar Rana <rgk.rana@gmail.com>
2021-04-26 16:18:12 +0200

    Updated for a2c

--- src/Algorithm/__init__.py
+++ src/Algorithm/__init__.py
@@ -1,10 +1,11 @@
+# TODO: Find technique to register the algorithms instead of manually importing them here
 from .base_algorithm import BaseAlgorithm
 from .on_policy import OnPolicyAlgorithm
 from .off_policy import OffPolicyAlgorithm
 from .dqn import DQN
 from .ddqn import DDQN
 from .reinforce import Reinforce
-from .actor_critic import ActorCritic
+from .a2c import A2C
 from .sarsa import Sarsa
 from .ddpg import DDPG
 from .sac import SAC
