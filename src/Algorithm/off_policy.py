6f60a4678539531818168263cad5a117ec560183
Raj Kumar Rana <rgk.rana@gmail.com>
2021-04-26 17:34:34 +0200

    Minor changes

--- src/Algorithm/off_policy.py
+++ src/Algorithm/off_policy.py
@@ -1,48 +1,60 @@
 from src.Buffer import ReplayBuffer
 from .base_algorithm import BaseAlgorithm
-from ..progressbar import ProgressBar
+from src.progressbar import ProgressBar
+from src.utils import get_scheduler
+
+from numpy import random
 
 
 class OffPolicyAlgorithm(BaseAlgorithm):
 
-    def __init__(self, *, num_init_exp_samples=None, buffer_size=800_000, **kwargs):
+    def __init__(self, *, batch_size_kwargs, num_init_exp_samples=None, max_init_exp_timestep="auto", buffer_size=1_000_000, **kwargs):
         super(OffPolicyAlgorithm, self).__init__(**kwargs)
-        self.num_init_exp_samples = int(num_init_exp_samples) if num_init_exp_samples is not None else num_init_exp_samples  # Number of initial exploration samples
         self.replay_buffer = ReplayBuffer(size=buffer_size)
-        self.scalar_summaries += ("buffer_size", )
+        self.batch_size_schedhuler = get_scheduler(batch_size_kwargs)
+        self.schedulers += (self.batch_size_schedhuler, )
+        self.num_init_exp_samples = None if num_init_exp_samples is None else int(num_init_exp_samples)
+        self.max_init_exp_timestep = self.max_episode_steps if max_init_exp_timestep == "auto" else max_init_exp_timestep
+        self.scalar_summaries += ("buffer_size", "batch_size")
 
-    def init_explore(self):
-        # Unwrap the original environment which does not record video
-        env = self.env.unwrapped
-        total_steps = self.num_init_exp_samples
-        pbar = ProgressBar(total_steps, title=f"# Collecting {total_steps} initial samples:", display_interval=100)
-        steps = 0
-        if self.load_model is None:
-            action_sample_func = lambda s: env.action_space.sample()  
-        else:
-            action_sample_func = lambda s: self.action(s)[0]
-        print()
-        while steps < total_steps:
-            done = False
-            state = env.reset()
-            while not done and (steps < total_steps):
-                action = action_sample_func(state)
-                next_state, reward, done, info = env.step(action)
-                self.transition = (state, action, reward, next_state, int(done))
-                self.replay_buffer.add(self.transition)
-                steps += 1
-                pbar.step()
-        env.reset()
-
-    def hook_before_train(self, **kwargs):
-        super(OffPolicyAlgorithm, self).hook_before_train(**kwargs)
-        if self.training:
-            if self.num_init_exp_samples is not None:
-                self.init_explore()
+    @property
+    def batch_size(self):
+        return int(self.batch_size_schedhuler.value)
 
     @property
     def buffer_size(self):
         return len(self.replay_buffer)
+
+    def init_explore(self, env, total_steps):
+        print()
+        steps = 0
+        pbar = ProgressBar(total_steps, title=f"# Collecting {total_steps} initial samples:")
+        while steps < total_steps:
+            done = False
+            state = env.reset()
+            epoch_steps = 0
+            while not done:
+                action = self.action(state)[0]  # env.action_space.sample()
+                next_state, reward, done, info = env.step(action)
+                self.transition = [state, action, reward, next_state, int(done)]
+                # env.render()
+                self.replay_buffer.add(self.transition)
+                epoch_steps += 1
+                pbar.step()
+                if ((steps + epoch_steps) >= total_steps) or (epoch_steps >= self.max_init_exp_timestep):
+                    break
+            steps += epoch_steps
+        env.reset()
+
+    def hook_after_step(self, **kwargs):
+        super().hook_after_step(**kwargs)
+        self.replay_buffer.add(self.transition)
+
+    def hook_before_train(self, **kwargs):
+        super(OffPolicyAlgorithm, self).hook_before_train(**kwargs)
+        if self.training and self.num_init_exp_samples is not None:
+            # Explore in the original environment which does not record video during rollout
+            self.init_explore(env=self.env.env, total_steps=self.num_init_exp_samples)
 
     def train(self):
         raise NotImplementedError
