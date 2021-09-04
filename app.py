1ccc7832c1287f78ff0d1b1caaec735e5a93146c
Raj K <rr-161039@hs-weingarten.de>
2021-09-04 12:11:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,56 @@
 import os
+import sys
 import subprocess
-import pandas as pd
 import streamlit as st
+from pyvirtualdisplay import Display
 
-sidebar = st.sidebar
+from test import setup
+
+def rollout(env, policy):
+  done = False
+  state = env.reset()
+  while not done:
+      action = policy(state)
+      next_state, reward, done, info = env.step(action)
+      state = next_state
+      state_img = env.render(mode="rgb_array")
+      yield state_img
+
+def execute(env_name, model_path, epochs, dump_path, include):
+  with Display(visible=False) as disp:
+    env, policy = setup(env_name, load_model, dump_path, include)
+    with st.empty():
+      for epoch in range(1, epochs+1):
+        for img in rollout(env, policy):
+          st.image(img, caption=f"Epoch {epoch}")
+
+
+@st.cache
+def get_os(platform):
+  if platform.startswith("linux"):
+    return "linux"
+  elif platform == "darwin":
+    return "osx"
+  return "windows"
+
 
 @st.cache
 def read_file(name, *args):
-	with open(name, *args) as fp:
-		return fp.read()
+  with open(name, *args) as fp:
+    return fp.read()
+
+
+# Constants
+OS = get_os(sys.platform)
+sbar = st.sidebar
 
 # Streamlit components
-env_name = sidebar.text_input("Environment ID", key="env_name_i")
-model_path = sidebar.text_input("Model path", key="model_path_i")
-epochs = sidebar.number_input("Epochs", 1, 10, key="epoch_i")
-include = sidebar.text_input("Additional imports", key="include_i")
-start_btn = sidebar.button("Start Testing", key="start_btn_i")
-# Video dump path
+env_name = sbar.text_input("Environment ID", key="env_name_i")
+model_path = sbar.text_input("Model path", key="model_path_i")
+epochs = sbar.number_input("Epochs", 1, 10, key="epoch_i")
+include = sbar.text_input("Additional imports", key="include_i")
+start_btn = sbar.button("Start Testing", key="start_btn_i")
+
 video_path = os.path.join("temp", env_name)
 
 # Descriptions
@@ -40,21 +74,32 @@ Parameter information:
 
 st.header("Rollout videos")
 
+st.info(f"OS: {OS.capitalize()}")
+
 if start_btn:
-	cmd = ["xvfb-run", "-a"]
-	cmd.extend(["python", "test.py", "--env_name", env_name, "--epochs", str(epochs), "--load_model", model_path, "--dump_path", video_path])
-	if include:
-		cmd.extend(["--include", include])
-	st.write("Executing the command:")
-	st.code(' '.join(cmd), language="shell")
-	st.info(f"Please be patient. \nTesting the policy in '{env_name}'.")
-	subprocess.run(cmd)
+  """cmd = []
+  if OS == "linux":
+    cmd.extend(["xvfb-run", "-a"])
+  cmd.extend([
+    "python", "test.py", 
+    "--env_name", env_name, 
+    "--epochs", str(epochs), 
+    "--load_model", model_path, 
+    "--dump_path", video_path,
+  ])
+  if include:
+    cmd.extend(["--include", include])
+  st.write("Executing the command:")
+  st.code(' '.join(cmd), language="shell")
+  st.info(f"Please be patient. \nTesting the policy in '{env_name}'.")
+  subprocess.run(cmd)"""
+  execute(env_name, model_path, epochs, video_path, include)
 
 if os.path.exists(video_path):
-	files = [os.path.join(video_path, filename) for filename in os.listdir(video_path) if filename.endswith(".mp4")]
-	for i, filename in enumerate(files, start=1):
-		st.subheader(f"Epoch {i}")
-		video_bytes = read_file(filename, "rb")
-		st.video(video_bytes)
+  files = [os.path.join(video_path, filename) for filename in os.listdir(video_path) if filename.endswith(".mp4")]
+  for i, filename in enumerate(files, start=1):
+    st.subheader(f"Epoch {i}")
+    video_bytes = read_file(filename, "rb")
+    st.video(video_bytes)
 else:
-	st.error("No video found!")
+  st.error("No video found!")
