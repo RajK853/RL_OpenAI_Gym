0ca26bf9b55b7ad138ca020f8268d5362bfb7531
Raj K <rr-161039@hs-weingarten.de>
2021-09-21 12:19:20 +0200

    Update off_policy.py
    - Add tqdm progress bar
    - Add default parameter values

--- src/Algorithm/off_policy.py
+++ src/Algorithm/off_policy.py
@@ -1,34 +1,46 @@
-from src.Buffer import ReplayBuffer
+import tqdm
+
 from .base_algorithm import BaseAlgorithm
-from src.progressbar import ProgressBar
+from src.Buffer import ReplayBuffer
 from src.utils import get_scheduler
 
 from numpy import random
 
+DEFAULT_KWARGS = {
+    "batch_size_kwargs": {
+        "type": "ConstantScheduler",
+        "value":  100,
+    },
+}
+
 
 class OffPolicyAlgorithm(BaseAlgorithm):
+    PARAMETERS = BaseAlgorithm.PARAMETERS.union({"batch_size_kwargs", "num_init_exp_samples", "max_init_exp_timestep", "buffer_size"})
 
-    def __init__(self, *, batch_size_kwargs, num_init_exp_samples=None, max_init_exp_timestep="auto", buffer_size=1_000_000, **kwargs):
+    def __init__(self, *, batch_size_kwargs=DEFAULT_KWARGS["batch_size_kwargs"], num_init_exp_samples=10000, max_init_exp_timestep="auto", 
+        buffer_size=1_000_000, **kwargs):
         super(OffPolicyAlgorithm, self).__init__(**kwargs)
+        self.buffer_size = buffer_size
         self.replay_buffer = ReplayBuffer(size=buffer_size)
+        self.batch_size_kwargs = batch_size_kwargs
         self.batch_size_schedhuler = get_scheduler(batch_size_kwargs)
         self.schedulers += (self.batch_size_schedhuler, )
         self.num_init_exp_samples = None if num_init_exp_samples is None else int(num_init_exp_samples)
         self.max_init_exp_timestep = self.max_episode_steps if max_init_exp_timestep == "auto" else max_init_exp_timestep
-        self.scalar_summaries += ("buffer_size", "batch_size")
+        self.scalar_summaries += ("replay_buffer_size", "batch_size")
 
     @property
     def batch_size(self):
         return int(self.batch_size_schedhuler.value)
 
     @property
-    def buffer_size(self):
+    def replay_buffer_size(self):
         return len(self.replay_buffer)
 
     def init_explore(self, env, total_steps):
         print()
         steps = 0
-        pbar = ProgressBar(total_steps, title=f"# Collecting {total_steps} initial samples:")
+        pbar = tqdm.tqdm(total=total_steps, desc="# Collecting initial samples")
         while steps < total_steps:
             done = False
             state = env.reset()
@@ -37,13 +49,13 @@ class OffPolicyAlgorithm(BaseAlgorithm):
                 action = self.action(state)[0]  # env.action_space.sample()
                 next_state, reward, done, info = env.step(action)
                 self.transition = [state, action, reward, next_state, int(done)]
-                # env.render()
                 self.replay_buffer.add(self.transition)    # TODO: Any problem in using hook_after_step?
                 epoch_steps += 1
-                pbar.step()
+                pbar.update()
                 if ((steps + epoch_steps) >= total_steps) or (epoch_steps >= self.max_init_exp_timestep):
                     break
             steps += epoch_steps
+            # pbar.refresh()
         env.reset()
 
     def hook_after_step(self, **kwargs):
